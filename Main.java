51d8e11997cb750952505e5b1472e3169eb2bb17
f.holka1@gmail.com <f.holka1@gmail.com>
2023-12-07 22:07:53 +0100

    third time test commit (for Main.java)

--- Main.java
+++ Main.java
@@ -3,25 +3,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Try out your class here
-        SimpleCollection j = new SimpleCollection("characters");
-        System.out.println(j);        
-        
-        System.out.println();
-        
-        j.add("magneto");
-        System.out.println(j);
-        
-        System.out.println();
-        
-        j.add("mystique");
-        System.out.println(j);
-        
-        System.out.println();
-        
-        j.add("phoenix");
-        System.out.println(j);
-        
+        Gift book = new Gift("Harry Potter and the Philosopher's Stone", 2);
+
+        Package gifts = new Package();
+        gifts.addGift(book);
+        System.out.println(gifts.totalWeight());
         
     }
 }
