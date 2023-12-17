c6761485a497d9324add1f0b364c79bf2c637d40
f.holka1@gmail.com <f.holka1@gmail.com>
2023-12-17 19:44:26 +0100

    All parts completed

--- Program.java
+++ Program.java
@@ -9,14 +9,8 @@ public class Program {
         GradeRegister register = new GradeRegister();
 
         UserInterface ui = new UserInterface(register, scanner);
-        //ui.start();
-        
-        register.addGradeBasedOnPoints(93);
-        register.addGradeBasedOnPoints(91);
-        register.addGradeBasedOnPoints(92);
-
-        System.out.println(register.averageOfPoints());
-        
+        ui.start();
+                
         
     }
 }
