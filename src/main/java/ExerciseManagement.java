ca9e704c5ede28cb7b731699ac5a83b332764fe3
f.holka1@gmail.com <f.holka1@gmail.com>
2023-12-18 23:49:04 +0100

    Completed the initial ExerciseManagementTest class and unit tests

--- src/main/java/ExerciseManagement.java
+++ src/main/java/ExerciseManagement.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
 public class ExerciseManagement {
 
+    private ArrayList<String> exercises;
+    
+    public ExerciseManagement(){
+        this.exercises = new ArrayList<>();
+    }
+    
+    public ArrayList<String> exerciseList(){
+        return this.exercises;
+    }
+    
+    public void add(String exercise){
+        this.exercises.add(exercise);
+    }
+    
 }
--- src/test/java/ExerciseManagementTest.java
+++ src/test/java/ExerciseManagementTest.java
@@ -4,5 +4,25 @@ import static org.junit.Assert.assertTrue;
 
 
 public class ExerciseManagementTest {
-
+    
+    @Test
+    public void exerciseListEmptyAtBeginning(){
+        ExerciseManagement management = new ExerciseManagement();
+        assertEquals(0, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne(){
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExerciseIsInList(){
+        ExerciseManagement management = new ExerciseManagement();
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+    
 }
