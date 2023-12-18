0139b572aac720b7db500b3400b73725b8457dc3
f.holka1@gmail.com <f.holka1@gmail.com>
2023-12-18 23:57:18 +0100

    Refactored Test class

--- src/test/java/ExerciseManagementTest.java
+++ src/test/java/ExerciseManagementTest.java
@@ -1,26 +1,30 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 
 
 public class ExerciseManagementTest {
+    private ExerciseManagement management;
+    
+    @Before
+    public void initialize(){
+        management = new ExerciseManagement();
+    }
     
     @Test
     public void exerciseListEmptyAtBeginning(){
-        ExerciseManagement management = new ExerciseManagement();
         assertEquals(0, management.exerciseList().size());
     }
     
     @Test
     public void addingExerciseGrowsListByOne(){
-        ExerciseManagement management = new ExerciseManagement();
         management.add("Write a test");
         assertEquals(1, management.exerciseList().size());
     }
     
     @Test
     public void addedExerciseIsInList(){
-        ExerciseManagement management = new ExerciseManagement();
         management.add("Write a test");
         assertTrue(management.exerciseList().contains("Write a test"));
     }
