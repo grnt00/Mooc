8ad1c04b8a93985ae05cb78c98bf9c77072a3141
grnt00 <f.holka1@gmail.com>
2024-12-24 15:15:11 +0100

    1st commit
    Branch - 09_02

---
+++ src/main/java/Person.java
@@ -0,0 +1,17 @@
+
+public class Person {
+    
+    protected String name;
+    protected String address;
+    
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+    
+    public String toString(){
+        return name + "\n" + "  " + address;
+    }
+    
+    
+}
