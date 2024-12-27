140ef874a574863a2b9d63dd175c113e678d7204
grnt00 <f.holka1@gmail.com>
2024-12-27 23:29:29 +0100

    Part2_1

---
+++ src/main/java/ChangeHistory.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class ChangeHistory {
+
+    private ArrayList<Double> history;
+        
+    public ChangeHistory(){
+        this.history = new ArrayList<>();
+    }
+    
+    public void add(double status){
+        this.history.add(status);
+    }
+    
+    public void clear(){
+        this.history.clear();
+    }
+    
+    public String toString(){
+        return this.history.toString();
+    }
+    
+}
