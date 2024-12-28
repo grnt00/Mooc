537afb55497135ed6df01e97d62c1797bb1b8b87
grnt00 <f.holka1@gmail.com>
2024-12-28 15:28:34 +0100

    Solution

--- src/main/java/ProductWarehouseWithHistory.java
+++ src/main/java/ProductWarehouseWithHistory.java
@@ -8,13 +8,30 @@ public class ProductWarehouseWithHistory extends ProductWarehouse {
         
         super(productName, capacity);
         this.productHistory = new ChangeHistory();
-        productHistory.add(initialBalance);
+        this.productHistory.add(initialBalance);
       // setting the initial balance as the warehouse balance
         super.addToWarehouse(initialBalance);   
     }
         
     public String history(){
-        return productHistory.toString();
+        return this.productHistory.toString();
     }
     
+    public void addToWarehouse(double amount) {
+       super.addToWarehouse(amount);
+       this.productHistory.add(super.getBalance());
+    }
+    
+    public double takeFromWarehouse(double amount){
+       this.productHistory.add(super.getBalance() - amount);
+       return super.takeFromWarehouse(amount);
+    }
+    
+    public void printAnalysis(){
+        System.out.println("Product: " + super.getName() + "\n" +
+                            "History: " + productHistory.toString() + "\n" +
+                            "Largest amount of product: " + this.productHistory.maxValue() + "\n" +
+                            "Smallest amount of product: " + this.productHistory.minValue() + "\n" +
+                            "Average: " + this.productHistory.average());       
+    }
 }
